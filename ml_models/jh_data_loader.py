f5b90ccdb211eeec746492c2232e6eeec767b571
102107806-JH <102107806@student.swin.edu.au>
2025-08-30 15:21:18 +1000

    Extracted data from excel file and it is now stored in a list

---
+++ ml_models/jh_data_loader.py
@@ -0,0 +1,90 @@
+import torch
+import torchvision
+from torch.utils.data import DataLoader, Dataset
+import numpy as np
+import math
+import openpyxl
+import re
+import csv
+import pandas
+
+class TrafficFlowDataSet(Dataset):
+    def __init__(self, data_set_file_name):
+
+        self._scats_site_to_one_hot = {}
+        self._days_to_values = {
+            "Monday": 0,
+            "Tuesday": 1,
+            "Wednesday": 2,
+            "Thursday": 3,
+            "Friday": 4,
+            "Saturday": 5,
+            "Sunday": 6
+        }
+
+        data_list = self._put_data_set_in_list(data_set_file_name)  # Store the data in a list #
+
+        data_list = self._format_data(data_list)
+
+        print_list_ds(data_list)
+
+
+
+
+
+
+    def __getitem__(self, item):
+        pass
+
+    def __len__(self):
+        pass
+
+    def _put_data_set_in_list(self, data_set_file_name):
+        pandas_data = pandas.read_excel(data_set_file_name)
+
+        data_list = []
+        scats_site_count = 0
+        for i in range(0, pandas_data.shape[0]):
+            scats_number = pandas_data['SCATS_Number'].iloc[i]
+            day = pandas_data['Weekday'].iloc[i]
+
+            if scats_number not in self._scats_site_to_one_hot:
+                self._scats_site_to_one_hot[scats_number] = scats_site_count
+                scats_site_count += 1
+
+            time = 0
+            for j in range(3, pandas_data.shape[1]):
+                tfv = pandas_data.iat[i, j]
+                data_list.append([scats_number, day, time, tfv])
+                time += 1
+
+        return data_list
+
+    def _format_data(self, data_list):
+        formated_data_list = []
+        for datum in data_list:
+            scats_number = datum[0]
+            day = datum[1]
+            time = datum[2]
+            tfv = datum[3]
+
+            formated_datum = [0] * len(self._scats_site_to_one_hot)
+            idx = self._scats_site_to_one_hot[scats_number]
+            formated_datum[idx] = 1
+
+            formated_datum.append(self._days_to_values[day])
+
+            formated_datum.append(time)
+
+            formated_datum.append(int(tfv))
+
+            formated_data_list.append(formated_datum)
+
+        return formated_data_list
+
+
+
+def print_list_ds(list):
+    for element in list:
+        print(element)
+
---
+++ sandbox.py
@@ -0,0 +1,4 @@
+from ml_models.jh_data_loader import TrafficFlowDataSet
+
+if __name__ == "__main__":
+    test = TrafficFlowDataSet("data/model_data.xlsx")
