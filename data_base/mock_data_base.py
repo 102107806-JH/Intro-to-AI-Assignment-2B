4c66fe37393b116cfdb138680f7f335659d26499
102107806-JH <102107806@student.swin.edu.au>
2025-09-05 03:40:02 +1000

    Updating the traffic flow predicitor to make it more readable

---
+++ data_base/mock_data_base.py
@@ -0,0 +1,62 @@
+import pandas
+from datetime import date, datetime, timedelta
+import math
+
+class CurrentData():
+    def __init__(self, initial_time, database_file_path):
+        self._current_data_dictionary = self._load_data_into_data_base(database_file_path, initial_time)
+
+    def _load_data_into_data_base(self, database_file_path, initial_time):
+        initial_time_rounded_down = datetime(year=initial_time.year,
+                                             month=initial_time.month,
+                                             day=initial_time.day,
+                                             hour=initial_time.hour,
+                                             minute=initial_time.minute - initial_time.minute % 15)
+
+        database_list = (pandas.read_excel(database_file_path).to_numpy()).tolist()
+        current_data_dictionary = {}  # Dictionary that will act as the database
+
+        # Create a key for each scats site and set the value to an empty list #
+        for entry in database_list:
+            scats_site = entry[0]
+
+            if scats_site not in current_data_dictionary:
+                current_data_dictionary[scats_site] = []
+
+            date = entry[2]
+
+            for time, i in enumerate(range(3, len(entry))):
+                tfv = entry[i]
+                entry_time = datetime(year=int(date[6:]),
+                                    month=int(date[3:5]),
+                                    day=int(date[:2]),
+                                    hour=math.floor(time * 15 / 60),
+                                    minute=time * 15 % 60)
+                if initial_time_rounded_down > entry_time:
+                    current_data_dictionary[scats_site].append([entry_time, tfv])
+
+
+        return current_data_dictionary  # Return the created database dictionary #
+
+    def get_scats_data(self, scats_site):
+        return self._current_data_dictionary[scats_site]
+
+    def get_input_sequence(self, scats_site, sequence_length):
+        scats_site_data = self._current_data_dictionary[scats_site]
+        final_data_sequence = scats_site_data[-sequence_length:]
+        return final_data_sequence
+
+    def append_data_to_scats_site(self, scats_site, data):
+        self._current_data_dictionary[scats_site].append(data)
+
+    def query(self, query_time, scats_site):
+        scats_site_data = self._current_data_dictionary[scats_site]
+
+        for entry in reversed(scats_site_data):  # Start searching in reverse #
+            bottom_time = self._db_entry_to_time_obj(entry)  # Get the time of the given entry #
+            top_time = bottom_time + timedelta(minutes=15)  # Get the maximum time for a query to be considered part of this prediction #
+
+            if query_time >= bottom_time and top_time > query_time:  # If the query time is between the two times then we have found the correct query #
+                tfv = entry[-1]  # The tfv is the final entry for the given entry #
+                return tfv
+        raise("QUERY TIME FOR SCATS SITE HAS NO CORRESPONDING ENTRY!")  # This should never happen #
---
+++ sandbox.py
@@ -0,0 +1,16 @@
+from path_finding.path_finder import PathFinder
+from file_handling.graph_vertex_edge_init import GraphVertexEdgeInit
+from datetime import date, datetime
+from jh_ml_models.flowrate_predictorV2 import FlowratePredictor
+if __name__ == "__main__":
+    current_time = datetime.now()
+    current_time = current_time.replace(month=8)  # We dont have access to data from current month faking that we are actually in the 8th month
+
+    flowrate_predictor = FlowratePredictor(initial_time=current_time,
+                                           sequence_length=12,
+                                           database_file_path="data/data_base.xlsx",
+                                           mode="GRU")
+
+    flowrate_predictor.get_data(time_since_initial_time=0, scats_site=2000)
+
+    print("Program End")
