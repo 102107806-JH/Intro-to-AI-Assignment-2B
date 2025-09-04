87dc58a9a03815eab64a1b5346ef1ab26f5a020a
102107806-JH <102107806@student.swin.edu.au>
2025-09-04 11:42:08 +1000

    Cleaning up file structure

--- OLD_FILES/path_finding_sandbox.py
+++ OLD_FILES/path_finding_sandbox.py
@@ -4,7 +4,7 @@ from search_algorithms.dijkstra_search import dijkstras_serach
 from jh_ml_models.flowrate_predictor import FlowratePredictor
 
 if __name__ == "__main__":
-    testFileExtractor = GraphVertexEdgeInit(r"data/graph_init_data.xlsx")
+    testFileExtractor = GraphVertexEdgeInit(r"../data/graph_init_data.xlsx")
     graph = testFileExtractor.extract_file_contents()  # Extract all the data from the graph init file and store in graph #
     flowrate_predictor = FlowratePredictor()  # init prediction model (this is a mock model for the time being but will be replaced with actual ones)
     test_problem = Problem(graph, initial_state=3662, goal_state=2000,flowrate_predictor=flowrate_predictor)  # Create the problem object which as an instance of one of the text-book abstractions #
