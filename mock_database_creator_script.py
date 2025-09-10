4e164347c60506a84fa92a2c90252a7ed9dbe313
102107806-JH <102107806@student.swin.edu.au>
2025-09-10 16:16:24 +1000

    Updating the  database creator so it can take the path to the excel file it creates as an arg

--- mock_database_creator_script.py
+++ mock_database_creator_script.py
@@ -1,4 +1,5 @@
 from misc.mock_data_base_creator import MockDataBaseCreator
 if __name__ == "__main__":
-    database = MockDataBaseCreator()
+    file_path_to_data_base = 'data/data_base.xlsx'
+    database = MockDataBaseCreator(file_path_to_data_base)
     database.write_new_excel_file()
