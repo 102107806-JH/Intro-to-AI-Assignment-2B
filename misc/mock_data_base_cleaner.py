d5030e756bb72f010fff61a49ba38374519a8ff5
102107806-JH <102107806@student.swin.edu.au>
2025-09-10 13:58:04 +1000

    Adding the data base cleaning. ALl -1s are set to 0 and any all zero days have thier time steps reset to the average of the given date

---
+++ misc/mock_data_base_cleaner.py
@@ -0,0 +1,67 @@
+import pandas
+import datetime
+import numpy as np
+import pandas as pd
+
+
+class MockDataBaseCleaner():
+    def __init__(self, path_to_database):
+        self._dataframe = pd.read_excel(path_to_database)
+
+    def process(self):
+        self._remove_negatives()
+        date_av_dictionary = self._get_average_value_for_each_date()
+        self._replace_zero_days_with_average(date_av_dictionary)
+        self._write_to_new_excel()
+
+    def _remove_negatives(self):
+        for i in range(self._dataframe.shape[0]):
+            for j in range(self._dataframe.shape[1]):
+                if type(self._dataframe.iat[i, j]) == np.int64 and self._dataframe.iat[i, j] < 0:
+                    self._dataframe.iat[i, j] = 0
+
+    def _get_average_value_for_each_date(self):
+        date_av_dictionary = {}
+        for i in range(self._dataframe.shape[0]):
+            date = self._dataframe.iat[i, 2]
+            if date not in date_av_dictionary:
+                date_av_dictionary[date] = []
+
+            row_tfv_list = []
+            # Calculate the row average. To determine if it needs to be included in the average
+            for j in range(3, self._dataframe.shape[1]):
+                row_tfv_list.append(self._dataframe.iat[i, j])
+
+            if np.average(np.asarray(row_tfv_list)) != 0:  # Exclude rows that contain only zeros #
+                date_av_dictionary[date].append(row_tfv_list)  # Is storing all valid entries for a given date
+
+
+        for date in date_av_dictionary:
+            date_av_dictionary[date] = np.asarray(date_av_dictionary[date])  # Convert to numpy array for easier processing #
+            date_av_dictionary[date] = np.mean(date_av_dictionary[date], axis=0)  # Convert the average for each time step #
+
+        return date_av_dictionary
+
+    def _replace_zero_days_with_average(self, date_av_dictionary):
+        for i in range(self._dataframe.shape[0]):
+            date = self._dataframe.iat[i, 2]
+            row_tfv_list = []
+
+            for j in range(3, self._dataframe.shape[1]):
+                row_tfv_list.append(self._dataframe.iat[i, j])
+
+            if np.average(np.asarray(row_tfv_list)) != 0:  # Row is not zero based and does not need replacing we can therfore continue to the next row #
+                continue
+
+            date_av_list = list(date_av_dictionary[date])  #Get the average for given date
+            for av_list_idx, j in enumerate(range(3, self._dataframe.shape[1])):
+                self._dataframe.iat[i, j] = int(date_av_list[av_list_idx])
+
+
+    def _write_to_new_excel(self):
+        self._dataframe.to_excel("data/cleaned_data_base.xlsx", index=False)
+
+
+
+
+
